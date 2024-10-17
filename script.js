c86d04d17f73e202a5834f086b81330d1820c246
CitedNome <70859144+CitedNome@users.noreply.github.com>
2024-10-17 18:42:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const sfx = [document.createElement("audio"), document.createElement("audio"), document.createElement("audio"), document.createElement("audio"), document.createElement("audio"), document.createElement("audio")];
+const lps = [document.createElement("audio"), document.createElement("audio"), document.createElement("audio"),document.createElement("audio"), document.createElement("audio"), document.createElement("audio")];
+
+var cur_lps = undefined;
+
+function playSFX(sfxId) {
+    sfx[sfxId].src = `sound${sfxId}.mp3`;
+    sfx[sfxId].play();
+    return console.log(sfxId);
+};
+
+function selectLPS(loopId) {
+    lps[loopId].loop = "true";
+    try {
+        clearCB();
+    } catch (error) {
+        
+    }
+    cur_lps = loopId;
+    // console.log(loopId);
+    return document.getElementsByClassName("loopBtn")[loopId].style.borderColor = "rgb(0, 255, 0)";
+};
+
+function ctrlSound(ctrlId) {
+    if (ctrlId == 0) { //Start
+        lps[cur_lps].src = `loop${cur_lps}.mp3`;
+        lps[cur_lps].play()
+    }
+    else {
+        lps[cur_lps].pause();
+        lps[cur_lps].load();
+        clearCB();
+        cur_lps = null;
+    }
+};
+
+//Utilities
+function clearCB() {
+var cBtns = document.getElementsByClassName("loopBtn");
+    for (let cBtn in cBtns) {
+        cBtns[cBtn].style.borderColor = "white"
+    }
+};
